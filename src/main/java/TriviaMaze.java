50895bbf0203934ebfff6fdbbd376ef76da7a3f7
Mathew Miller <mathew.evan85@gmail.com>
2024-10-24 20:06:06 -0700

    restructured the java project

--- src/main/java/TriviaMaze.java
+++
@@ -1,6 +0,0 @@
-public class TriviaMaze {
-
-    public static void main(String[] args) {
-        System.out.println("Trivia Maze");
-    }
-}
--- src/model/Item.java
+++ src/model/Item.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.sql.Struct;
-
 /**
  * Models an Item in the maze.
  */
