9291ef7a6a19d8eff1f7f4cdf3d37012ae8a5779
Mathew Miller <mathew.evan85@gmail.com>
2024-11-02 14:56:20 -0700

    Adding the database of questions for the game

---
+++ src/main/java/controller/QuestionFactory.java
@@ -0,0 +1,17 @@
+package controller;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+import org.sqlite.SQLiteDataSource;
+
+public class QuestionFactory {
+
+
+    public QuestionFactory() {
+
+
+    }
+
+
+}
---
+++ src/main/resources/questions/questions.sql
@@ -0,0 +1,195 @@
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('What is the most popular Christmas carol of all time?',
+        'Silent Night',
+        'Jingle Bells',
+        'Deck the Halls',
+        'We Wish You a Merry Christmas');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('The tradition of putting up Christmas trees originated in Germany.',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('What is the name of the reindeer with a red nose?',
+        'rudolph');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('In which year was the first Christmas card sent?',
+        '1843',
+        '1853',
+        '1863',
+        '1873');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('Santa Claus is also known as Saint Nicholas.',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('What do people traditionally put on top of a Christmas tree?',
+        'star');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('Which of these movies is NOT a Christmas movie',
+        'Four Rooms',
+        'Die Hard',
+        'Gremlins',
+        'Batman Returns');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('The abbreviation ''Xmas'' is derived from Greek.',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('What is the name of the Grinch''s dog in the Dr. Seuss book ''How the Grinch Stole Christmas''?',
+       'max');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('Which Christmas movie features a character named ''Kevin McCallister?',
+        'Home Alone',
+        'Elf',
+        'The Santa Clause',
+        'Miracle on 34th Street');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('Eggnog was originally created in America.',
+        FALSE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('In the song ''Jingle Bells'', a one horse ____ sleigh',
+        'open');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('In Charles Dickens'' ''A Christmas Carol'', what is Mr. Scrooge''s first name?',
+        'Ebenezer',
+        'Jacob',
+        'Thomas',
+        'William');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('The poinsettia plant was named after Joel Poinsett, who introduced the plant to the United States.',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('What is the name of the town where the grinch tries to ruin christmas',
+        'whoville');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('What is the name of the main character in the movie ''Elf?',
+        'Buddy',
+        'Santa',
+        'Jack',
+        'Charlie');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('Christmas is celebrated on December 25th in all countries.',
+        FALSE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('What is the French word for Christmas?',
+        'noel');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('Which beverage company was the first to use Santa Claus in an advertisement?',
+        'Coca-Cola',
+        'Pepsi',
+        'Dr. Pepper',
+        'Mountain Dew');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('Rudolph the Red-Nosed Reindeer was created as a marketing gimmick for a department store.',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('In ''The Night Before Christmas'' poem nothing was stirring not even a ___?',
+        'mouse');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('Which country has a tradition of filling children''s shoes with gifts on Christmas Eve?',
+        'Netherlands',
+        'Spain',
+        'Italy',
+        'France');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('The song ''Jingle Bells'' was originally written for Thanksgiving.',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('What Christmas decoration is made from a parasitic plant?',
+        'mistletoe');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('In the song ''Frosty the Snowman'', what is Frosty''s nose made of?',
+        'A button',
+        'A carrot',
+        'A rock',
+        'A piece of coal');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('The Twelve Days of Christmas end on January 6th.',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('In ''The Twelve Days of Christmas'', how many swans are swimming?',
+        '7');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('Who wrote the song ''White Christmas?',
+        'Irving Berlin',
+        'Frank Sinatra',
+        'Bing Crosby',
+        'Nat King Cole');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('The star of Bethlehem is a symbol of Christmas?',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('In which country is it a tradition to eat KFC for Christmas dinner?',
+        'japan');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('Which of the following is one of Santa''s reindeer?',
+        'Comet',
+        'Blixem',
+        'Dapper',
+        'Vickson');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('St. Nicholas is the patron saint of sailors.',
+        TRUE);
+
+INSERT INTO text_input_question (question, answer)
+VALUES ('What band had the hit single ''Last Christmas?',
+        'wham!');
+
+INSERT INTO multiple_choice_question (question, answer, wrong_answer_1, wrong_answer_2, wrong_answer_3)
+VALUES ('What is the name of the little girl in the Nutcracker ballet?',
+        'Clara',
+        'Marie',
+        'Elizabeth',
+        'Sophia');
+
+INSERT INTO boolean_question (question, answer)
+VALUES ('Christmas trees were originally decorated with edible items.',
+        TRUE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
